aa834eaf36badcc1eb00947664c35cb1d29c04e6
ruks2022 <https://github.com/ruks2022/instagram_clone.git>
2023-09-07 22:54:14 +0530

    home page

---
+++ assets/mainThree.js
@@ -0,0 +1,35 @@
+// Stepper Form
+
+var current = 0;
+var tabs = $(".tab");
+var tabs_pill = $(".tab-pills");
+
+loadFormData(current);
+
+function loadFormData(n) {
+  $(tabs_pill[n]).addClass("active");
+  $(tabs[n]).removeClass("d-none");
+  $("#back_button").attr("disabled", n == 0 ? true : false);
+  n == tabs.length - 1
+    ? $("#next_button").text("Submit").removeAttr("onclick")
+    : $("#next_button")
+        .attr("type", "button")
+        .text("Next")
+        .attr("onclick", "next()");
+}
+
+function next() {
+  $(tabs[current]).addClass("d-none");
+  $(tabs_pill[current]).removeClass("active");
+
+  current++;
+  loadFormData(current);
+}
+
+function back() {
+  $(tabs[current]).addClass("d-none");
+  $(tabs_pill[current]).removeClass("active");
+
+  current--;
+  loadFormData(current);
+}
